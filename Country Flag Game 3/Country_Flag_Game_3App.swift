c27b81c2a222ecc9f8d368a40afd872e7cf32fa8
Alan Rivera <arivera@bsd220.org>
2024-01-18 09:43:20 -0600

    Initial Commit

---
+++ Country Flag Game 3/Country_Flag_Game_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_Game_3App.swift
+//  Country Flag Game 3
+//
+//  Created by Alan Rivera on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_Game_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
